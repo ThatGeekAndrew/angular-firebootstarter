b57c39326002306ec9c369c4f9fd1f3f5c8650d7
Andrew Alsante <andrewalsante@gmail.com>
2018-07-31 03:16:18 -0400

    Converted nav to bootstrap

--- src/app/ui/main-nav/main-nav.component.ts
+++ src/app/ui/main-nav/main-nav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../core/auth.service';
+
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'main-nav',
@@ -6,13 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-nav.component.scss']
 })
 export class MainNavComponent {
+  closeResult: string;
 
-  show = false;
+  constructor(public auth: AuthService, private modalService: NgbModal) { }
 
-  constructor() { }
+  open(content: any) {
+    this.modalService.open( content, {
+      size: 'sm',
+      centered: true,
+      windowClass: 'animated fadeIn',
+      // backdropClass: 'blurred'
+    }).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
 
-  toggleCollapse() {
-    this.show = !this.show;
+      if (result == 'logout') {
+        this.auth.signOut();
+      }
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
   }
 
 }
